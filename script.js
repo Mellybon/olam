96d34e6bea10edf878f40b75552bbac76b776b4c
Mellybon <95433747+Mellybon@users.noreply.github.com>
2024-11-01 17:11:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const gif = document.querySelector(".gif");
 
 // Change text and gif when the Yes button is clicked
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "I LOVE YOU TOOOO MY OLENKA 😘)";
+  question.innerHTML = "I LOVE YOU TOOOO MY OLENKA 😘";
   gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNXdhN2ViMmpyMXQwaXBpa2Fic2EwemYwODl4OTQyOXNkajhmdTVrMyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/26FLdmIp6wJr91JAI/giphy.gif";
 });
 
